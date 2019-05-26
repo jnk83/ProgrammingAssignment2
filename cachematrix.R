1cd0aa0951a35cf17e53bc1409140aaf9b76e196
jnk83 <51059242+jnk83@users.noreply.github.com>
2019-05-26 16:46:19 -0400

    ProgAss2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a function that calculates the inverse of a Matrix.  Values can then be manipulated
+## and cached thereafter directly from the object, instead of being forced to call makeCacheMatrix again.
+## cacheSolve creates a function that checks to see if the inverse value of a Matrix is already cached in
+## memory.  If so, the value is returned.  If not, cacheSolve calculates the inverse and caches the values.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a space in memory initially for a null matrix 'm' for later inversion.
+## 'set' allows for later re-setting the matrix values through makeCacheMatrix directly. 'get' returns the 
+## matrix values provided by the user. 'sinvert' sets the value of the inverse matrix and caches it. 
+## 'ginvert' fetches the cached inverse matrix.  'list' creates a list and attaches the proper values to allow
+## for use of the $ operator.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        sinvert <- function(solve) m <<- solve
+        ginvert <- function() m
+        list(set = set, get = get, sinvert = sinvert, ginvert = ginvert)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve first checks to see if a cached version of m exists by attempting to get 
+## the values of the inverse matrix from makeCacheMatrix above.  The function checks to see
+## if the value is null.  If not null, cacheSolve returns the stored values.  If no cached values exist,
+## cacheSolve solves for the inverse, caches the values, and displays them to the user.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$ginvert()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$sinvert(m)
+        m
 }
